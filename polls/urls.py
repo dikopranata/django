6d4c4c0fca21646e8ac781e83ebe02e32a48d491
diko pranata <dikocapo@gmail.com>
2023-11-23 10:12:00 +0700

    template: klik to detail room

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
+    path("room/<str:pk>/",views.room, name="room"),
+    path("home/",views.home, name="home")
 ]
--- polls/views.py
+++ polls/views.py
@@ -9,13 +9,30 @@ posts = [{
     'author' : 'Mitchell Admin', 'title':'Title 2', 'content':'00d0d0d0d0dd'
 }]
 
+rooms = [{
+    'id' : 1, 'name' : 'Diko'
+}, {
+    'id' : 2, 'name' : 'Pranata'
+},{
+    'id' : 3, 'name' : 'John'
+}]
+
 def index(request):
-    context = {'posts' : posts }
+    context = {'posts' : posts, 'title': 'Judul', 'rooms':rooms }
     #context = { 'posts' : Post.objects.all }
     return render(request, 'polls/index.html', context)
 def about(request):
     context = { 'posts' : Post.objects.all }
     return render(request, 'polls/about.html', context)
-
+def home(request):
+    context = {'rooms': rooms }
+    return render(request,'polls/home.html',context)
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room' : room}
+    return render(request,'polls/room.html',context)
 # def about(request):
 #     return HttpResponse("Hello, Django. You're at the ABout.", {'title':'About'})
